9f1aa0ca0503e2c9d7196f377dca4835ea98fdce
Christopher Ochs <ochscs@mail.uc.edu>
2018-03-03 15:10:40 -0500

    Added autho keys

--- twitterbot.py
+++ twitterbot.py
@@ -1,5 +1,8 @@
 import tweepy
 
+consumer_key = 	"0j5zlz4h5IQ71ncz8ZsrpTEMc"
+consumer_secret = 	"zJpRVvpMGI13c1MnDrzAKC6fwIIpoUJa25YAFAV4B9aovKDdwe"
+
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
