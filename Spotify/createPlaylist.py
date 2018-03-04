c73375547a198dda98deeb808d1a6bb41c8bfeb8
Kyle Bernard O'Connor <thekyleoconnor@gmail.com>
2018-03-03 23:21:05 -0500

    Song Search and song class

--- Spotify/createPlaylist.py
+++ Spotify/createPlaylist.py
@@ -1,7 +1,10 @@
 import auth
 import spotipy
+import searchSpotify
 
-def createNewPlaylist(twitterObject):
+def createNewPlaylist():
 	spotify = spotipy.Spotify(auth=auth.token)
-	if token:
-		playlist = spotify.user_playlist_create(auth.username,twitterHandle + "'s Custom Playlist", public=True,"This is a public playlist custom made for "+twitterHandle)
+	if auth.token:
+		word="happy day night crazy west tomorrow"
+		
+		#playlist = spotify.user_playlist_create(auth.username,twitterHandle + "'s Custom Playlist", public=True,"This is a public playlist custom made for "+twitterHandle)
--- Spotify/searchSpotify.py
+++ Spotify/searchSpotify.py
@@ -1,11 +1,22 @@
 import spotipy
 import auth
+import json
+import songClass
 
-results = spotify.artist_albums(birdy_uri, album_type='album')
-albums = results['items']
-while results['next']:
-    results = spotify.next(results)
-    albums.extend(results['items'])
+def searchForSongs(word):
+	spotify = spotipy.Spotify(auth=auth.token)
+	results = spotify.search(word, limit=1, offset=0,type='track')
+	#file = open("outputfile.txt", 'w')
+	#file.write(json.dumps(results))
+	trackStr = ''
+	tracks = results['tracks']['items']
+	for x in range(10):
+		results = spotify.next(results['tracks'])
+		tracks.extend(results['tracks']['items'])
+	for track in tracks:
+		trackStr += track['name']
+		trackStr += ' '
+	print(trackStr)
 
-for album in albums:
-    print(album['name'])
+
+
