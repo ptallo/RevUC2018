c66c71bb9574697e13899956e81397a38caa1b52
Unknown <diehmsr@mail.uc.edu>
2018-03-03 22:12:32 -0500

    code for ibm

---
+++ revuc2018-env/curl test.py
@@ -0,0 +1,25 @@
+import json
+from watson_developer_cloud import NaturalLanguageUnderstandingV1
+from watson_developer_cloud.natural_language_understanding_v1 \
+  import Features, EntitiesOptions, KeywordsOptions
+
+natural_language_understanding = NaturalLanguageUnderstandingV1(
+  username='aa518a49-7b68-4e96-a208-e82fe33784d1',
+  password='tOC1RWTjAm0v',
+  version='2017-02-27')
+
+response = natural_language_understanding.analyze(
+  text='IBM is an American multinational technology company '
+       'headquartered in Armonk, New York, United States, '
+       'with operations in over 170 countries.',
+  features=Features(
+    entities=EntitiesOptions(
+      emotion=True,
+      sentiment=True,
+      limit=2),
+    keywords=KeywordsOptions(
+      emotion=True,
+      sentiment=True,
+      limit=2)))
+
+print(json.dumps(response, indent=2))
